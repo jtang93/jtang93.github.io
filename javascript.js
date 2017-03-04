9a1423f13c25bc408c9eedc8f898068a1df70a29
jtang93 <tang.jonathan93@gmail.com>
2017-03-04 13:15:04 -0500

    Visual rehaul
    Added landing page, javascript for navbar and smooth scroll

---
+++ javascript.js
@@ -0,0 +1,33 @@
+(function($) {
+
+$(document).ready(function(){
+
+	$(".fixed-nav-bar").hide();	
+	
+	$(function(){
+		$(window).scroll(function(){
+			if($(this).scrollTop()>450) {
+				$(".fixed-nav-bar").fadeIn();
+			} 
+			else {
+				$(".fixed-nav-bar").fadeOut();
+			}
+		});
+	});
+});
+
+$(document).ready(function(){
+	$(".scroll").on('click', function(event) {
+		if(this.hash !== "") {
+			event.preventDefault();
+			
+			var hash = this.hash;
+			
+			$("html, body").animate({
+				scrollTop: $(hash).offset().top
+			}, 800, 'swing');
+			
+			}
+	});
+});
+})(jQuery);
